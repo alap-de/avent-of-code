68a4afeafeb708dee5254d18e16f36f6ff08919f
Alap De <alap.de@gmail.com>
2023-12-13 19:45:02 +0530

    Complete day 1, 2023

--- 2023/day-1/Trebuchet.js
+++ 2023/day-1/Trebuchet.js
@@ -14,30 +14,67 @@ In this example, the calibration values of these four lines are 12, 38, 15, and 
 Consider your entire calibration document. What is the sum of all of the calibration values?
 */
 
-const fs = require("fs");
-const readline = require("readline");
+const { readFileSync } = require("fs");
 
-const nums = [];
-const rl = readline.createInterface({
-  input: fs.createReadStream("Trebuchet_input.txt", { encoding: "utf8" }),
-});
+const numMap = {
+  one: "o1e",
+  two: "t2o",
+  three: "t3e",
+  four: "f4r",
+  five: "f5e",
+  six: "s6x",
+  seven: "s7n",
+  eight: "e8t",
+  nine: "n9e",
+};
 
-rl.on("line", (chunk) => {
-  const numbers = chunk.match(/\d+/g);
-  let allNums = numbers.join("");
-  if (allNums.length == 1) {
-    allNums += allNums;
+/**
+ * Replaces words in a string with their corresponding values.
+ * The words can be reused. for example 'oneight' will be replaced with '18'.
+ *
+ * @param {string} str - The input string to be modified.
+ * @return {string} The modified string with words replaced.
+ */
+const replaceWords = (str) => {
+  for (let key of Object.keys(numMap)) {
+    str = str.replaceAll(key.toString(), numMap[key].toString());
   }
 
-  let result = `${allNums.charAt(0)}${allNums.charAt(allNums.length - 1)}`;
-  nums.push(Number(result));
-});
+  return str;
+};
 
-rl.on("close", () => {
-  const sum = nums.reduce((acc, val) => acc + val, 0);
-  console.log(sum);
-});
+/**
+ * Calculates the sum of all elements in an array.
+ *
+ * @param {Array} arr - The array of numbers.
+ * @return {number} The sum of all elements in the array.
+ */
+const sum = (arr) => arr.reduce((a, b) => a + b, 0);
 
-rl.on("error", (err) => {
-  console.error(err);
-});
+/**
+ * Parses a line and returns the first and last number in the line.
+ *
+ * @param {string} line - The line to parse.
+ * @return {number} The first and last number in the line.
+ */
+const parseLine = (line) => {
+  const allNums = line.replaceAll(/[a-zA-Z]/g, "");
+  return Number(`${allNums[0]}${allNums[allNums.length - 1]}`);
+};
+
+/**
+ * Parses the given text and returns an array of parsed lines.
+ *
+ * @param {string} text - The text to be parsed.
+ * @return {Array} An array of parsed numbers from the lines.
+ */
+const parseText = (text) => {
+  const lines = text.split("\n");
+  return lines.map((line) => parseLine(line));
+};
+const data = readFileSync("./input.txt", "utf8");
+
+// Part 1 solution
+console.log(sum(parseText(data)));
+// Part 2 solution
+console.log(sum(parseText(replaceWords(data))));
